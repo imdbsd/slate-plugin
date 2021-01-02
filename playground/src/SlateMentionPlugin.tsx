64b7561e1d1c67650a6ae6fa016eac4c50c80679
surya darma <budi.surya@kumparan.com>
2021-01-02 15:37:18 +0800

    update story

--- playground/src/SlateMentionPlugin.tsx
+++ playground/src/SlateMentionPlugin.tsx
@@ -1,27 +1,16 @@
 import {useState, useMemo, FC} from 'react'
 import {createEditor, Node} from 'slate'
-import {Slate, Editable, withReact} from 'slate-react'
-// @ts-ignore
-import withMention from 'slate-mention-plugin'
+import {Slate, Editable, withReact, DefaultLeaf} from 'slate-react'
+import {decorate, RenderLeaf} from 'slate-mention-plugin'
 
 const Editor: FC<any> = () => {
-  const editor = useMemo(() => withMention(withReact(createEditor())), [])
+  const editor = useMemo(() => withReact(createEditor()), [])
   const [value, setValue] = useState<Node[]>([
     {
       type: 'paragraph',
       children: [
         {
-          text: `Slate paste url example, try block some text and paste url or github url to the blocked text.
-            `,
-        },
-      ],
-    },
-    {
-      type: 'paragraph',
-      children: [
-        {
-          text:
-            'To change how the url rendered, edit the renderElement in SlatePasteUrl.tsx',
+          text: `coba mention `,
         },
       ],
     },
@@ -33,7 +22,16 @@ const Editor: FC<any> = () => {
       value={value}
       onChange={(newValue) => setValue(newValue)}
     >
-      <Editable />
+      <Editable
+        renderLeaf={(props) => {
+          return <RenderLeaf {...props} /> || <DefaultLeaf {...props} />
+        }}
+        decorate={(entry) => {
+          const range = decorate(editor)(entry)
+          console.log({range})
+          return range
+        }}
+      />
     </Slate>
   )
 }
