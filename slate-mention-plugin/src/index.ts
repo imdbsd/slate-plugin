2bc3d552f063086d9677977bce231ea102b3d1bb
surya darma <budi.surya@kumparan.com>
2021-01-03 14:52:04 +0800

    fix build and remove withMention

--- slate-mention-plugin/src/index.ts
+++ slate-mention-plugin/src/index.ts
@@ -1,5 +1,4 @@
 export const MENTION_LEAF = 'mention-leaf'
 
 export {default as RenderLeaf} from './RenderLeaf'
-export {default as withMention} from './withMention'
 export {default as decorate} from './decorate'
